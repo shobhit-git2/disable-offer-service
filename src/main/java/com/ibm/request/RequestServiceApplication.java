eaeae641fb4bafbe9f5d0df3d740505582a368f4
shobhit-git2 <shobhit51924@gmail.com>
2020-02-25 23:03:37 +0530

    Update RequestServiceApplication.java

--- src/main/java/com/ibm/request/RequestServiceApplication.java
+++ src/main/java/com/ibm/request/RequestServiceApplication.java
@@ -2,15 +2,11 @@ package com.ibm.request;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class RequestServiceApplication {
 
 	public static void main(String[] args) {
